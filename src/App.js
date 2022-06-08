a1feaf0f616a407445bed3585ead5caea36b1d6b
AhmadAfifM <ahmad.afifm@gmail.com>
2022-06-08 22:43:26 +0700

    fixed login page

--- src/App.js
+++ src/App.js
@@ -4,14 +4,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // Collecting Import Pages
 import Login from "./pages/Login";
-import Register from "./pages/Register";
+// import Register from "./pages/Register";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
